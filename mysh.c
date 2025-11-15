8b607c2108f39822567b8c95b59dd3b5868d1934
Oluwademilade Alao <oaa100@assembly.cs.rutgers.edu>
2025-11-15 17:27:51 -0500

    done

--- mysh.c
+++ mysh.c
@@ -133,7 +133,6 @@ void cd(char *pathname)
     if (check < 0)
     {
         fprintf(stderr, "Failed to change directory\n");
-        exit(1);
     }
 }
 
@@ -237,7 +236,7 @@ int generalCommands(arraylist_t args, int fd)
     }
     else if (strcmp(cmd, "cd") == 0)
     {
-        if (args.len -1 != 2)
+        if (args.len - 1 != 2)
         {
             fprintf(stderr, "Invalid arguments\n");
         }
@@ -271,7 +270,12 @@ int generalCommands(arraylist_t args, int fd)
     }
     else
     {
-        int redirect = 0;
+        int redirectOut = 0;
+        int redirectIn = 0;
+        int pipeCheck = 0;
+        int conditional = 0;
+        arraylist_t args2;
+        al_init(&args2, 10);
         char *fileName;
         int fd2;
         for (int i = 0; i < args.len - 1; i++)
@@ -280,16 +284,192 @@ int generalCommands(arraylist_t args, int fd)
             if (strcmp(s, ">") == 0)
             {
                 fileName = args.data[i + 1];
-                redirect = 1;
-                break;
+                redirectOut = 1;
+                for (int j = i - 2; j < args.len; j++)
+                {
+                    char **s = malloc(sizeof(char **));
+                    al_pop(&args, s);
+                    free(s);
+                }
+                al_push(&args, NULL);
+                // break;
+            }
+            if (strcmp(s, "<") == 0)
+            {
+                fileName = args.data[i + 1];
+                redirectIn = 1;
+                for (int j = i - 2; j < args.len; j++)
+                {
+                    char **s = malloc(sizeof(char **));
+                    al_pop(&args, s);
+                    puts("popped");
+                    free(s);
+                }
+                al_push(&args, NULL);
+                // break;
+            }
+            if (strcmp(s, "|") == 0)
+            {
+                pipeCheck = 1;
+                char **temp = malloc(sizeof(char **));
+                al_pop(&args, temp);
+                free(temp);
+                for (int j = i + 1; j < args.len; j++)
+                {
+                    char *s = args.data[j];
+                    al_push(&args2, s);
+                }
+                for (int i = args.len - 1; i > args2.len - 1; i--)
+                {
+                    temp = malloc(sizeof(char **));
+                    al_pop(&args, temp);
+                    free(temp);
+                }
+                al_push(&args2, NULL);
+                al_push(&args, NULL);
+                char *cmd1 = args.data[0];
+                char *cmd2 = args2.data[0];
+                char *path1 = which(cmd1);
+                char *path2 = which(cmd2);
+                int pfd[2];
+                pipe(pfd);
+
+                if (fork() == 0)
+                {
+                    dup2(pfd[1], STDOUT_FILENO);
+                    close(pfd[1]);
+                    close(pfd[0]);
+
+                    execv(path1, args.data);
+                    perror(path1);
+                    exit(EXIT_FAILURE);
+                }
+
+                if (fork() == 0)
+                {
+                    dup2(pfd[0], STDIN_FILENO);
+                    close(pfd[0]);
+                    close(pfd[1]);
+
+                    execv(path2, args2.data);
+                    perror(path2);
+                    exit(EXIT_FAILURE);
+                }
+
+                close(pfd[1]);
+                close(pfd[0]);
+
+                wait(NULL);
+                wait(NULL);
+                return 0;
+            }
+            if (strcmp(s, "or") == 0)
+            {
+                arraylist_t newArgs;
+                al_init(&newArgs, 10);
+                for (int j = i + 1; j < args.len - 1; j++)
+                {
+                    char *s = args.data[j];
+                    al_push(&newArgs, s);
+                }
+                char **temp = malloc(sizeof(char **));
+                al_pop(&args, temp);
+                free(temp);
+                for (int i = args.len - 1; i > newArgs.len - 1; i--)
+                {
+                    temp = malloc(sizeof(char **));
+                    al_pop(&args, temp);
+                    free(temp);
+                }
+
+                al_push(&newArgs, NULL);
+                al_push(&args, NULL);
+                char *cmd1 = args.data[0];
+                char *cmd2 = newArgs.data[0];
+
+                char *path1 = which(cmd1);
+                char *path2 = which(cmd2);
+                pid_t child = fork();
+                if (child == 0)
+                {
+                    execv(path1, args.data);
+                    perror(path1);
+                    exit(EXIT_FAILURE);
+                }
+
+                int status;
+                child = wait(&status);
+                if (WIFSIGNALED(status))
+                {
+                    pid_t child = fork();
+                    if (child == 0)
+                    {
+                        execv(path2, newArgs.data);
+                        perror(path2);
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                wait(NULL);
+                return 0;
+            }
+            else if (strcmp(s, "and") == 0)
+            {
+                arraylist_t newArgs;
+                al_init(&newArgs, 10);
+                for (int j = i + 1; j < args.len - 1; j++)
+                {
+                    char *s = args.data[j];
+                    al_push(&newArgs, s);
+                }
+
+                char **temp = malloc(sizeof(char **));
+                al_pop(&args, temp);
+                free(temp);
+                for (int i = args.len - 1; i > newArgs.len - 1; i--)
+                {
+                    temp = malloc(sizeof(char **));
+                    al_pop(&args, temp);
+                    free(temp);
+                }
+                al_push(&newArgs, NULL);
+                al_push(&args, NULL);
+
+                char *cmd1 = args.data[0];
+                char *cmd2 = newArgs.data[0];
+
+                char *path1 = which(cmd1);
+                char *path2 = which(cmd2);
+                pid_t child = fork();
+                if (child == 0)
+                {
+                    execv(path1, args.data);
+                    perror(path1);
+                    exit(EXIT_FAILURE);
+                }
+
+                int status;
+                child = wait(&status);
+                if (WIFEXITED(status))
+                {
+                    pid_t child = fork();
+                    if (child == 0)
+                    {
+                        execv(path2, newArgs.data);
+                        perror(path2);
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                wait(NULL);
+                return 0;
             }
         }
+
         char *cmdName = args.data[0];
         char *pathName = which(cmdName);
         pid_t child = fork();
         if (child == 0)
         {
-            if (redirect == 1)
+            if (redirectOut == 1)
             {
                 fd2 = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, 0640);
                 if (dup2(fd2, STDOUT_FILENO) == -1)
@@ -298,17 +478,23 @@ int generalCommands(arraylist_t args, int fd)
                 }
                 close(fd2);
             }
-            execv(pathName, args.data);
-            for (int i = 0; i < args.len - 1; i++)
+            else if (redirectIn == 1)
             {
-                puts(args.data[i]);
+                fd2 = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, 0640);
+                if (dup2(fd2, STDIN_FILENO) == -1)
+                {
+                    exit(1);
+                }
+                close(fd2);
             }
+            execv(pathName, args.data);
             perror(pathName);
             exit(EXIT_FAILURE);
         }
         int status;
         child = wait(&status);
-        // int fd3 = open(fileName, O_RDONLY);
+
+        al_destroy(&args2);
     }
     if (args.len - 1 != 0)
     {
